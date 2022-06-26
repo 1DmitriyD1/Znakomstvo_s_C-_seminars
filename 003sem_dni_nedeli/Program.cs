41f10b7e45e06d8ff08fd36081c753ab96051e2a
1DmitriyD1 <1dxgt1@gmail.com>
2022-06-26 17:27:17 +0300

    init commit

---
+++ 003sem_dni_nedeli/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+class Program
+{
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+static void Main(string[] args)
+{
+Console.WriteLine("INPUT DAY NUMBER: ");
+int number = (Convert.ToInt32(Console.ReadLine())) % 7;
+string day = "";
+if( number == 1) day = "ponedyelnik";
+if( number == 2) day = "vtornik";
+if( number == 3) day = "sreda";
+if( number == 4) day = "chetverg";
+if( number == 5) day = "pyatniza";
+if( number == 6) day = "subbota";
+if( number == 7) day = "voskresenje";
+Console.WriteLine($"Номер дня соответствует: {day}");
+string[] day2 = {"PN", "VTor", "SRed", "CHT", "PTN", "SB", "VS"};
+Console.WriteLine($"Номер дня соответствует: {day2[number-1]}");
+}
+
+}
+}
