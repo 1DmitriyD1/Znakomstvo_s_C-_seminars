69177285effc21d641165977d1b897e9e48afe17
1DmitriyD1 <1dxgt1@gmail.com>
2022-06-26 22:51:30 +0300

    init commit, DZ zad_8

---
+++ DZ_sem01_zadacha8/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Zadacha_8
+{
+class Program
+    {
+        // Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. (не включая N)
+
+        // 5 -> 2, 4
+        // 8 -> 2, 4, 6
+       
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Программа показывает все чётные числа от 1 до N. (не включая N)");
+            Console.Write("Введите целое число N = ");
+            
+            int number_n = Convert.ToInt32(Console.ReadLine());
+            
+            int index = 2;
+            
+            Console.WriteLine("Чётные числа:");
+
+            while (index < number_n) 
+                {
+                
+                Console.Write($"{index}, ");
+                
+                index = index + 2;
+
+                }
+                
+        }
+    }
+}
