af2630c271bfecabf1ae394cf1c18e31cef2dc3b
1DmitriyD1 <1dxgt1@gmail.com>
2022-06-26 15:37:25 +0300

    init commit

---
+++ 001sem/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld
+{
+class Program
+    {
+        //Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число: ");
+            // 1. Получаем число
+            int number = Convert.ToInt32(Console.ReadLine());
+            // 2. Умножаем и записываем в отдельную переменную
+            // int square = number * number;
+            // // Или 2. Получаем квадрат в переменной number
+            // number *= number;
+
+            // Получаем квадрат
+            int square = Convert.ToInt32(Math.Pow(number, 2));
+            // 3. Вывод результата
+            Console.WriteLine($"Квадрат числа: {square}");
+
+        }
+    }
+}
