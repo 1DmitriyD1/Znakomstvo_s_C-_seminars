1d5ba95660c8fd4b2f698f55aaf8771bbbd839e4
1DmitriyD1 <1dxgt1@gmail.com>
2022-06-26 17:16:55 +0300

    init commit

---
+++ 002sem/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+class Program
+{
+static void Main(string[] args)
+    {
+
+        Console.Clear();
+        Console.WriteLine("Введите первое число, второе число: ");
+        
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        int square = number2 * number2;
+
+        if (square == number1)
+            {
+                Console.WriteLine($"Первое число {number1} является квадратом второго {number2}");
+            }
+            else
+            {
+                Console.WriteLine($"Первое число {number1} не является квадратом второго {number2}");
+            }
+    }
+
+}
+}
